50cd288bbae38d6bff10551060717da3780c899a
Aruuke <domovoi111805@gmail.com>
2022-11-29 17:54:15 +0600

    hw_6

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blogs.urls')),
+    path('', include('blogs.urls')),
     path('users/', include('users.urls'))
 ]
